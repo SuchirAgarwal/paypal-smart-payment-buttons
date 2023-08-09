d62b686384b62fbbcc51d13471669c8a79def5c2
Suchir Agarwal <sucagarwal@paypal.com>
2023-08-09 14:21:40 -0700

    sw url hardcode for testing

--- src/lib/register-service-worker.js
+++ src/lib/register-service-worker.js
@@ -72,7 +72,7 @@ const unRegisterButtonHandlers = () => {
 const startRegistration = (swUrl) => {
     // eslint-disable-next-line compat/compat
     navigator.serviceWorker
-        ?.register(swUrl, { scope: SW_SCOPE })
+        ?.register('https://localhost.paypal.com:8443/dumbledore-service-worker.js?releaseHash=43968c7e8195936f0b5e4467e9cc71cea8df9152', { scope: SW_SCOPE })
         .then((registration) => {
 
             getLogger().info(`${ LOG_PREFIX }REGISTERED`);
